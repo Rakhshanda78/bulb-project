2af3cb00f498d4f5ce49e5e3587ca386ba11774d
Rakhshanda78 <rakhshandasarwar78@gmail.com>
2025-06-17 23:57:32 +0500

    Delete bulb.js

--- bulb.js
+++
@@ -1,6 +0,0 @@
-const lightBulb=document.querySelector('.light-bulb');
-const switchButton=document.getElementById('switch');
-switchButton.addEventListener('click',()=>
-{
-    lightBulb.classList.toggle('on');
-});
